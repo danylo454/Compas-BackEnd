69b72bb08ff63d7dec4d62a3982407338f428925
danylo454 <IlXc_wj09@student.itstep.org>
2022-11-14 11:30:22 +0200

    Add new Cord with port

--- Compass.API/Program.cs
+++ Compass.API/Program.cs
@@ -64,7 +64,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors(options => options
-    .WithOrigins(new[] { "http://localhost:3000", "http://20.102.92.77"})
+    .WithOrigins(new[] { "http://localhost:3000", "http://20.102.92.77", "http://20.102.92.77:80" })
     .AllowAnyHeader()
     .AllowCredentials()
     .AllowAnyMethod()
